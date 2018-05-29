3da526af583c7ea499475e7f566bfca9660af064
Thurler <gt@anlix.io>
2018-05-29 00:21:26 -0300

    Added csv file writing

--- zabbix_history_filter.js
+++ zabbix_history_filter.js
@@ -1,9 +1,11 @@
+const fs = require('fs');
 const Promise = require('promise');
 const request = require('request');
 
 let zabbixHost = 'zabbixgui.gigalink.net.br';
 let tokenAuth = null;
 let zabbixData = {};
+let metrics = {};
 
 let timeFrom = new Date(2018, 4, 4, 0, 0, 0).getTime() / 1000;
 let timeUntil = new Date(2018, 4, 4, 23, 59, 59).getTime() / 1000;
@@ -97,7 +99,8 @@ getDeviceData = function(device) {
       }
       if (!body.result) {
         console.log('\nWARNING! Error on request for device data:');
-        console.log('Device: ' + device.name);
+        console.log('Device ID: ' + device.hostid);
+        console.log('Device Name: ' + device.name);
         console.log('No results in body: ' + body.toString() + '\n');
         resolve();
         return;
@@ -111,6 +114,9 @@ parseDeviceData = function(result, device) {
   let items = result[0].items;
   let promises = [];
   items.forEach((item, i)=>{
+    if (!item.key_.match(/dhcpd.hosts.tx/)) {
+      metrics[item.key_] = true;
+    }
     zabbixData[device.name][item.key_] = {'name': item.name};
     promises.push(getDataHistory(device, item));
   });
@@ -140,7 +146,8 @@ getDataHistory = function(device, item) {
     (error, response, body)=>{
       if (error) {
         console.log('\nWARNING! Error on request for data history:');
-        console.log('Device: ' + device.name);
+        console.log('Device ID: ' + device.hostid);
+        console.log('Device Name: ' + device.name);
         console.log('Item: ' + item.name);
         console.log(error + '\n');
         resolve();
@@ -159,6 +166,51 @@ parseDataHistory = function(results, device, item) {
   });
 };
 
+writeCSVFiles = function(metrics, data) {
+  for (let metric in metrics) {
+    if (!Object.prototype.hasOwnProperty.call(metrics, metric)) continue;
+    let fsMetric = metric.replace(/\[/g, '(');
+    fsMetric = fsMetric.replace(/\]/g, ')');
+    fsMetric = fsMetric.replace(/\//g, ';');
+    let file = fs.createWriteStream('output/' + fsMetric + '.csv');
+    file.once('open', ()=>{
+      file.write('mac,timestamp,value\n');
+      for (let mac in data) {
+        if (!Object.prototype.hasOwnProperty.call(data, mac)) continue;
+        if (!(metric in data[mac])) continue;
+        for (let clock in data[mac][metric]) {
+          if (clock === 'name') continue;
+          if (Object.prototype.hasOwnProperty.call(data[mac][metric], clock)) {
+            file.write(mac+','+clock+','+data[mac][metric][clock]+'\n');
+          }
+        }
+      }
+      file.end();
+    });
+  }
+  let file = fs.createWriteStream('output/dhcp-hosts.csv');
+  file.once('open', ()=>{
+    file.write('mac,lease_name,lease_mac,timestamp,tx_value\n');
+    for (let mac in data) {
+      if (!Object.prototype.hasOwnProperty.call(data, mac)) continue;
+      for (let key in data[mac]) {
+        if (!Object.prototype.hasOwnProperty.call(data[mac], key)) continue;
+        if (!key.match(/dhcpd.hosts.tx/)) continue;
+        for (let clock in data[mac][key]) {
+          if (Object.prototype.hasOwnProperty.call(data[mac][key], clock)) {
+            if (clock === 'name') continue;
+            file.write(mac + ',');
+            file.write(data[mac][key]['name'] + ',');
+            file.write(key.substr(15, 17) + ',');
+            file.write(clock + ',');
+            file.write(data[mac][key][clock] + '\n');
+          }
+        }
+      }
+    }
+  });
+};
+
 main = function() {
   process.on('unhandledRejection', (reason)=>{
     console.log(reason);
@@ -173,16 +225,20 @@ main = function() {
     let promises = [];
     let hosts = result[0].hosts;
     hosts.forEach((host, i)=>{
-      if (host.hostid == '10301' || host.hostid == '10302') {
-        zabbixData[host.name] = {};
-        promises.push(getDeviceData(host));
-      }
+      // if (host.hostid !== '10354') return;
+      zabbixData[host.name] = {};
+      promises.push(getDeviceData(host));
     });
     return Promise.all(promises);
   })
   .then((results)=>{
+    console.log('Writing result to file...');
+    fs.writeFile('./resultData.json', JSON.stringify(zabbixData), (err)=>{
+      if (err) console.log(err);
+    });
+    console.log('Writing csv files...');
+    writeCSVFiles(metrics, zabbixData);
     console.log('Done');
-    console.log(zabbixData);
   }, (reason)=>{
     console.log(reason);
   });
