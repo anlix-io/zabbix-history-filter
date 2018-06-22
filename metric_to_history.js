4ba0d5599f4cc29c7e5b5964c0075310184bd111
Thurler <gt@anlix.io>
2018-06-22 17:10:14 -0300

    Added more float metrics

--- metric_to_history.js
+++ metric_to_history.js
@@ -9,6 +9,12 @@
 let metricToHistory = {
   'active.burst.ping.avg': 0,
   'active.burst.ping.loss': 3,
+  'system.cpu.load[percpu,avg1]': 0,
+  'system.cpu.util[,system]': 0,
+  'system.cpu.util[,user]': 0,
+  'wifi.iwinfo.snr[wlan0]': 0,
+  'wifi.iwinfo.noise[wlan0]': 0,
+  'wifi.iwinfo.signal[wlan0]': 0,
 };
 
 module.exports = metricToHistory;
